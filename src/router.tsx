28a3dacebdc2dbbda1b601d739a2a2a6138e7f70
gonawon-crh <gonawon@gmail.com>
2025-03-21 22:30:45 +0800

    更新vite.config.ts和router.tsx配置

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import App from './App';
 import Home from './pages/Home';
 import PersonalInfo from './pages/PersonalInfo';
 import PaymentRecord from './pages/PaymentRecord';
 import Result from './pages/Result';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <App />,
